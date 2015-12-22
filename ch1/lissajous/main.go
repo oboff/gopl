4602b1865f9860eb4ec7688b4b837f003e116aa6
srbufi <orlando.boffill@gmx.com>
2015-12-23 08:31:11 +1100

    ex1.7

---
+++ ch1/lissajous/main.go
@@ -0,0 +1,53 @@
+// main.go
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
+	"os"
+)
+
+var palette = []color.Color{color.White, color.Black}
+
+const (
+	whiteIndex = 0 //first color
+	blackIndex = 1 //next color
+)
+
+func main() {
+	lissajous(os.Stdout)
+}
+
+func lissajous(out io.Writer) {
+	const (
+		cycles  = 5     // number of x oscillator revolutions
+		res     = 0.001 // angular resolution
+		size    = 100   // image canvas covers [-size..+size]
+		nframes = 128   // number of animation frames
+		delay   = 3     // delay between frames in 10ms units
+	)
+	freq := rand.Float64() * 3.0 // relative freq of y osci
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 //phase diff
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	err := gif.EncodeAll(out, &anim) // Ignore encode err
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v", err)
+	}
+}
